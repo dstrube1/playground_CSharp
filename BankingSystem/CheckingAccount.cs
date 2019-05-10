82cf9610fd5c6e87aff72a00b8e00c8f93069aba
David Strube <dstrube@gmail.com>
2019-05-10 13:51:34 -0400

    Simple example Banking System

---
+++ BankingSystem/CheckingAccount.cs
@@ -0,0 +1,11 @@
+﻿namespace BankingSystem
+{
+    public class CheckingAccount : Account
+    {
+        public CheckingAccount() : base()
+        { }
+        public CheckingAccount(decimal funds, string accountName, AccountType accountType) : base(funds, accountName, accountType)
+        { }
+
+    }
+}
---
+++ BankingSystem/SavingsAccount.cs
@@ -0,0 +1,11 @@
+﻿namespace BankingSystem
+{
+    public class SavingsAccount : Account
+    {
+        public SavingsAccount() : base()
+        { }
+
+        public SavingsAccount(decimal funds, string accountName, AccountType accountType) : base(funds, accountName, accountType)
+        { }
+    }
+}
