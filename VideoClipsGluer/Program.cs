2bf58ab12ee34be20306396e06eaff7e32f1e075
David Strube <dstrube1@users.noreply.github.com>
2021-07-20 14:05:35 -0400

    Trying to get this to work on new computer

--- VideoClipsGluer/Program.cs
+++ VideoClipsGluer/Program.cs
@@ -4,13 +4,70 @@ using System.Diagnostics;
 using System.IO;
 using System.Text;
 
+/// <summary>
+/// Glues video clips together.
+/// 
+/// To Run on a Mac, must run in Visual Studio for Mac. Running in VS Code is hard, more effort than it's worth.
+/// Also, run these in Terminal:
+/// brew update
+/// brew upgrade
+/// brew install ffmpeg
+///
+/// Also, if getting this error:
+/// illegal short term buffer state detected
+///
+/// Then:
+/// ???
+///
+/// --enable-shared
+/// --enable-pthreads
+/// --enable-version3
+/// --enable-ffplay
+/// --enable-gnutls
+/// --enable-gpl
+/// --enable-libaom
+/// --enable-libbluray
+/// --enable-libdav1d
+/// --enable-libmp3lame
+/// --enable-libopus
+/// --enable-librav1e
+/// --enable-librubberband
+/// --enable-libsnappy
+/// --enable-libsrt
+/// --enable-libtesseract
+/// --enable-libtheora
+/// --enable-libvidstab
+/// --enable-libvorbis
+/// --enable-libvpx
+/// --enable-libwebp
+/// --enable-libx264
+/// --enable-libx265
+/// --enable-libxml2
+/// --enable-libxvid
+/// --enable-lzma
+/// --enable-libfontconfig
+/// --enable-libfreetype
+/// --enable-frei0r
+/// --enable-libass
+/// --enable-libopencore-amrnb
+/// --enable-libopencore-amrwb
+/// --enable-libopenjpeg
+/// --enable-libspeex
+/// --enable-libsoxr
+/// --enable-libzmq
+/// --enable-libzimg
+/// --disable-libjack
+/// --disable-indev=jack
+/// --enable-avresample
+/// --enable-videotoolbox
+/// </summary>
 namespace VideoClipsGluer
 {
     class Program
     {
         static void Main(string[] args)
         {
-            const bool stopAtFirstDir = false;
+            const bool stopAtFirstDir = true;
 
             string workingDir;
             if (args.Length < 1 || args[0] == "")
@@ -25,28 +82,28 @@ namespace VideoClipsGluer
             var dirs = new ArrayList(Directory.GetDirectories(workingDir));
             dirs.Sort();
             dirs.TrimToSize();
+            const bool allInOne = true;
             foreach (var dir in dirs)
             {
                 Console.WriteLine($"dir: {dir}");
-                var mp4files = new ArrayList(Directory.GetFiles(dir.ToString(), "*.mp4"));
-                //processFileType("all", mp4files, dir.ToString());
-
-                var mp4filesFront = new ArrayList(Directory.GetFiles(dir.ToString(), "*-front.mp4"));
-                processFileType("front", mp4filesFront, dir.ToString());
-
-                var mp4filesLeft = new ArrayList(Directory.GetFiles(dir.ToString(), "*-left_repeater.mp4"));
-                processFileType("left", mp4filesLeft, dir.ToString());
-
-                var mp4filesRight = new ArrayList(Directory.GetFiles(dir.ToString(), "*-right_repeater.mp4"));
-                processFileType("right", mp4filesRight, dir.ToString());
-
-                var mp4filesBack = new ArrayList(Directory.GetFiles(dir.ToString(), "*-back.mp4"));
-                processFileType("back", mp4filesBack, dir.ToString());
-
-                if (mp4files.Count == 1)
+                if (allInOne)
                 {
-                    //there is only 1 so skip this folder
-                    continue;
+                    var mp4files = new ArrayList(Directory.GetFiles(dir.ToString(), "*.mp4"));
+                    processFileType("all", mp4files, dir.ToString());
+                }
+                else
+                {
+                    var mp4filesFront = new ArrayList(Directory.GetFiles(dir.ToString(), "*-front.mp4"));
+                    processFileType("front", mp4filesFront, dir.ToString());
+
+                    var mp4filesLeft = new ArrayList(Directory.GetFiles(dir.ToString(), "*-left_repeater.mp4"));
+                    processFileType("left", mp4filesLeft, dir.ToString());
+
+                    var mp4filesRight = new ArrayList(Directory.GetFiles(dir.ToString(), "*-right_repeater.mp4"));
+                    processFileType("right", mp4filesRight, dir.ToString());
+
+                    var mp4filesBack = new ArrayList(Directory.GetFiles(dir.ToString(), "*-back.mp4"));
+                    processFileType("back", mp4filesBack, dir.ToString());
                 }
                 if (stopAtFirstDir)
                 {
@@ -86,6 +143,11 @@ namespace VideoClipsGluer
         {
             files.TrimToSize();
             files.Sort();
+            if (files.Count < 2)
+            {
+                Console.WriteLine("Files list is empty or just 1. Skipping " + fileType);
+                return;
+            }
             Console.WriteLine($"{fileType} file count: {files.Count}");
             //TODO Only combine files from the same day
             //If multiple days exist in 1 folder, make a separate file for each day
