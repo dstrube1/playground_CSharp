535de2177aa5b2d64b287ddabf5cea1c8c03a1c5
David Strube <dstrube3@gatech.edu>
2025-05-26 09:21:03 -0400

    Minor edits

--- ConsoleApplication1/ConsoleApplication1/obj/Debug/net7.0/ConsoleApplication1.AssemblyInfo.cs
+++ ConsoleApplication1/ConsoleApplication1/obj/Debug/net7.0/ConsoleApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d53ac720d15ca39d7dff94672d77aad74e082fb1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9f632170e5b3f667faa59f1acc900e21d6780b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
